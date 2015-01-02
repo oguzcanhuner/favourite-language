254dc8bdb39144d696c312f7efc531ccc73227f0
Oguz Huner <oguzcanhuner@gmail.com>
2015-01-02 17:49:01 +0000

    init with working test

---
+++ Rakefile
@@ -0,0 +1,5 @@
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.pattern = "spec/test_*.rb"
+end
---
+++ spec/test_working.rb
@@ -0,0 +1,7 @@
+require_relative 'spec_helper'
+
+class TestWorking < Minitest::Test
+  def test_true_is_true
+    assert_equal true, true
+  end
+end
